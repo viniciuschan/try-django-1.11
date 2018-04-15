a790cc4fa2ac446305d9170cab9079d8aa40e7ae
Vinicius Chan <viniciuschan@hotmail.com>
2018-04-15 18:45:58 -0300

    Added generic views

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
-# Create your models here.
+from .utils import unique_slug_generator
+
+class RestaurantLocation(models.Model):
+	name 		= models.CharField(max_length=120)
+	location 	= models.CharField(max_length=120, null=True, blank=True)
+	category 	= models.CharField(max_length=120, null=True, blank=True)
+	timestamp 	= models.DateTimeField(auto_now_add=True)
+	updated 	= models.DateTimeField(auto_now=True)
+	slug		= models.SlugField(null=True, blank=True)
+
+	def __str__(self):
+		return self.name
+
+	@property
+	def title(self):
+		return self.name
+
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = unique_slug_generator(instance)
+
+# def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
+# 	print('saved!')
+# 	print(instance.timestamp)
+# 	if not instance.slug:
+# 		instance.slug = unique_slug_generator(instance)
+# 		instance.save()
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+# post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,36 +1,45 @@
-import random
-
+from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
-class HomeView(TemplateView):
-	template_name = 'home.html'
+from .models import RestaurantLocation
 
-	def get_context_data(self, *args, **kwargs):
-		context = super(HomeView, self).get_context_data(*args, **kwargs)
-		num = random.randint(0, 10000)
 
-		context = {
-			'num': num,
-		}
+def restaurant_listview(request,):
+	template_name = 'restaurants/restaurants_list.html'
+	queryset = RestaurantLocation.objects.all()
+	context = {
+		'object_list': queryset
+	}
+	return render(request, template_name, context)
 
-		return context		
 
-class AboutView(TemplateView):
-	template_name = 'about.html'
+class RestaurantListView(ListView):
+	template_name = 'restaurants/restaurants_list.html'
 
-	def get_context_data(self, *args, **kwargs):
-		context = super(AboutView, self).get_context_data(*args, **kwargs)
-		
-		return context		
+	def get_queryset(self):
+		slug = self.kwargs.get("slug")
+		if slug:
+			queryset = RestaurantLocation.objects.filter(
+					Q(category__iexact=slug) |
+					Q(category__icontains=slug)
+				)
+		else:
+			queryset = RestaurantLocation.objects.all()
 
-class ContactView(TemplateView):
-	template_name = 'contact.html'
+		return queryset
 
-	def get_context_data(self, *args, **kwargs):
-		context = super(ContactView, self).get_context_data(*args, **kwargs)
-		
-		return context		
+class RestaurantDetailView(DetailView):
+	template_name = 'restaurants/restaurants_detail.html'
+	queryset = RestaurantLocation.objects.all()
+
+	# def get_context_data(self, *args, **kwargs):
+	# 	context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+	# 	return context
+
+	# def get_object(self, *args, **kwargs):
+	# 	rest_id = self.kwargs.get('rest_id')
+	# 	obj = get_object_or_404(RestaurantLocation, pk=rest_id)
+	# 	return obj
